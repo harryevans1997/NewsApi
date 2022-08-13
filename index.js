304525f0c9cdb2c18a36737578e0125e34495523
Harry Evans <harryevans177@gmail.com>
2022-08-13 19:31:13 +0100

    removed console logs, added GET handler that filters results by source (e.g wsj/thetimes), changed source name to be more RESTful

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 
 const newspapers = [
   {
-    name: "The Wall Street Journal",
+    name: "wsj",
     address: "https://www.wsj.com/news/sports",
     base: "",
   },
@@ -16,29 +16,32 @@ const newspapers = [
 const articles = [];
 
 newspapers.forEach((newspaper) => {
-  axios.get(newspaper.address).then((response) => {
-    const html = response.data;
-    const $ = cheerio.load(html);
+  axios
+    .get(newspaper.address)
+    .then((response) => {
+      const html = response.data;
+      const $ = cheerio.load(html);
 
-    // searched each <a> and checks if the first child (and descendants) contains any of the sports keywords
-    $("a > :first-child", html).each(function () {
-      const spanText = $(this).text().toLowerCase();
+      // searched each <a> and checks if the first child (and descendants) contains any of the sports keywords
+      $("a > :first-child", html).each(function () {
+        const spanText = $(this).text().toLowerCase();
 
-      sportsJargon.forEach((word) => {
-        if (spanText.includes(word)) {
-          const title = $(this).text();
-          const url = $(this).parents().attr("href");
-          if (!articles.find((obj) => obj.title === title)) {
-            articles.push({
-              title,
-              url: newspaper.base + url,
-              source: newspaper.name,
-            });
+        sportsJargon.forEach((word) => {
+          if (spanText.includes(word)) {
+            const title = $(this).text();
+            const url = $(this).parents().attr("href");
+            if (!articles.find((obj) => obj.title === title)) {
+              articles.push({
+                title,
+                url: newspaper.base + url,
+                source: newspaper.name,
+              });
+            }
           }
-        }
+        });
       });
-    });
-  });
+    })
+    .catch((err) => console.log(err));
 });
 
 app.get("/", (req, res) => {
@@ -46,9 +49,45 @@ app.get("/", (req, res) => {
 });
 
 app.get("/news", (req, res) => {
-  console.log(res);
-  console.log(sportsJargon);
   res.json(articles);
+});
+
+app.get("/news/:newspaperId", (req, res) => {
+  const newspaperId = req.params.newspaperId;
+
+  const newspaperAddress = newspapers.filter(
+    (newspaper) => newspaper.name == newspaperId
+  )[0].address;
+  const newspaperBase = newspapers.filter(
+    (newspaper) => newspaper.name == newspaperId
+  )[0].base;
+
+  axios
+    .get(newspaperAddress)
+    .then((response) => {
+      const html = response.data;
+      const $ = cheerio.load(html);
+      const filteredArticles = [];
+
+      $("a > :first-child", html).each(function () {
+        const spanText = $(this).text().toLowerCase();
+        sportsJargon.forEach((word) => {
+          if (spanText.includes(word)) {
+            const title = $(this).text();
+            const url = $(this).parents().attr("href");
+            if (!filteredArticles.find((obj) => obj.title === title)) {
+              filteredArticles.push({
+                title,
+                url: newspaperBase + url,
+                source: newspaperId,
+              });
+            }
+          }
+        });
+      });
+      res.json(filteredArticles);
+    })
+    .catch((err) => console.log(err));
 });
 
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`));
